b0db25b62bb1381eee2122f87e56bb874b5c9364
game <hhhau1910@gmail.com>
2026-04-18 14:03:42 +0700

    update nvim config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,5 @@
 local map = vim.keymap.set
 
--- ══════════════════════════════════════
--- General
--- ══════════════════════════════════════
-
 map("n", "<C-s>", "<cmd>w<cr>", { desc = "Save file" })
 
 map("n", "<C-q>", "<cmd>q<cr>", { desc = "Quit" })
@@ -11,10 +7,6 @@ map("n", "<C-q>", "<cmd>q<cr>", { desc = "Quit" })
 map({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yank" })
 
 map("v", "p", [["_dP]], { desc = "Paste without losing clipboard" })
-
--- ══════════════════════════════════════
--- Navigation
--- ══════════════════════════════════════
 
 map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
@@ -24,33 +16,19 @@ map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 map("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move line down" })
 map("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move line up" })
 
--- Giữ cursor ở giữa khi scroll
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down (centered)" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up (centered)" })
 
--- Giữ cursor ở giữa khi tìm kiếm
 map("n", "n", "nzzzv", { desc = "Next search (centered)" })
 map("n", "N", "Nzzzv", { desc = "Prev search (centered)" })
 
--- ══════════════════════════════════════
--- Buffers
--- ══════════════════════════════════════
 map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Close buffer" })
 
--- ══════════════════════════════════════
--- Terminal
--- ══════════════════════════════════════
 map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- ══════════════════════════════════════
--- Misc
--- ══════════════════════════════════════
-
--- Thay thế word dưới cursor toàn file
-map("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word under cursor" })
-
--- Thoát terminal mode
 map("n", "<leader>th", "<cmd>split | terminal<cr>", { desc = "Open terminal horizontal" })
 map("n", "<leader>tv", "<cmd>vsplit | terminal<cr>", { desc = "Open terminal vertical" })
+
+map("n", "<leader>rn", vim.lsp.buf.rename, {})
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -10,7 +10,6 @@ return {
       },
     },
   },
-
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -24,6 +23,58 @@ return {
             "--completion-style=detailed",
           },
         },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    lazy = true,
+    cmd = "ConformInfo",
+    keys = {
+      {
+        "<leader>cF",
+        function()
+          require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+        end,
+        mode = { "n", "x" },
+        desc = "Format Injected Langs",
+      },
+    },
+    init = function()
+      LazyVim.on_very_lazy(function()
+        LazyVim.format.register({
+          name = "conform.nvim",
+          priority = 100,
+          primary = true,
+          format = function(buf)
+            require("conform").format({ bufnr = buf })
+          end,
+          sources = function(buf)
+            local ret = require("conform").list_formatters(buf)
+            return vim.tbl_map(function(v)
+              return v.name
+            end, ret)
+          end,
+        })
+      end)
+    end,
+    opts = {
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false,
+        quiet = false,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        sh = { "shfmt" },
+        java = { "google-java-format" },
+        cpp = { "clang-format" },
+        c = { "clang-format" },
+      },
+      formatters = {
+        injected = { options = { ignore_errors = true } },
       },
     },
   },
